51dbf9a726f150b5dd981dd2409be1c6334fecfd
Maciej <thehelleye@gmail.com>
2019-01-17 00:02:30 +0100

    finished kinda

--- Zad4.sql
+++ Zad4.sql
@@ -309,30 +309,6 @@ VALUES (Plebs(11, (SELECT REF(kot) FROM okocury kot WHERE kot.pseudo = 'MALY')))
 VALUES (Plebs(12, (SELECT REF(kot) FROM okocury kot WHERE kot.pseudo = 'MALA')))
 SELECT *
 FROM dual;
-/*INSERT INTO oplebs
-VALUES (Plebs(1, (SELECT REF(kot) FROM okocury kot WHERE kot.pseudo = 'SZYBKA')));
-INSERT INTO oplebs
-VALUES (Plebs(2, (SELECT REF(kot) FROM okocury kot WHERE kot.pseudo = 'BOLEK')));
-INSERT INTO oplebs
-VALUES (Plebs(3, (SELECT REF(kot) FROM okocury kot WHERE kot.pseudo = 'LASKA')));
-INSERT INTO oplebs
-VALUES (Plebs(4, (SELECT REF(kot) FROM okocury kot WHERE kot.pseudo = 'MAN')));
-INSERT INTO oplebs
-VALUES (Plebs(5, (SELECT REF(kot) FROM okocury kot WHERE kot.pseudo = 'DAMA')));
-INSERT INTO oplebs
-VALUES (Plebs(6, (SELECT REF(kot) FROM okocury kot WHERE kot.pseudo = 'PLACEK')));
-INSERT INTO oplebs
-VALUES (Plebs(7, (SELECT REF(kot) FROM okocury kot WHERE kot.pseudo = 'RURA')));
-INSERT INTO oplebs
-VALUES (Plebs(8, (SELECT REF(kot) FROM okocury kot WHERE kot.pseudo = 'ZERO')));
-INSERT INTO oplebs
-VALUES (Plebs(9, (SELECT REF(kot) FROM okocury kot WHERE kot.pseudo = 'PUSZYSTA')));
-INSERT INTO oplebs
-VALUES (Plebs(10, (SELECT REF(kot) FROM okocury kot WHERE kot.pseudo = 'UCHO')));
-INSERT INTO oplebs
-VALUES (Plebs(11, (SELECT REF(kot) FROM okocury kot WHERE kot.pseudo = 'MALY')));
-INSERT INTO oplebs
-VALUES (Plebs(12, (SELECT REF(kot) FROM okocury kot WHERE kot.pseudo = 'MALA')));*/
 --</editor-fold>
 
 --</editor-fold>
@@ -451,8 +427,8 @@ CREATE OR REPLACE TYPE BODY KONTO IS
     MEMBER FUNCTION isOn
         RETURN NUMBER IS BEGIN
         IF data_usuniecia IS NOT NULL THEN
-        RETURN 1;
-            END IF;
+            RETURN 1;
+        END IF;
         RETURN 0;
     END;
 END;
@@ -564,42 +540,97 @@ CONSTRAINT ooi_sc_p poszkodowany SCOPE IS okocury
 );
 
 --<editor-fold desc="Dane incydentów">
-INSERT ALL
-    INTO oIncydenty VALUES (1,(SELECT REF(k) FROM oKocury k WHERE k.pseudo='TYGRYS'), 'KAZIO', '2004-10-13 ', 'USILOWAL NABIC NA WIDLY')
-    INTO oIncydenty VALUES (2,(SELECT REF(k) FROM oKocury k WHERE k.pseudo='ZOMBI'), 'SWAWOLNY DYZIO', '2005-03-07 ', 'WYBIL OKO Z PROCY')
-    INTO oIncydenty VALUES (3,(SELECT REF(k) FROM oKocury k WHERE k.pseudo='BOLEK'), 'KAZIO', '2005-03-29 ', 'POSZCZUL BURKIEM')
-    INTO oIncydenty VALUES (4,(SELECT REF(k) FROM oKocury k WHERE k.pseudo='SZYBKA'), 'GLUPIA ZOSKA', '2006-09-12 ', 'UZYLA KOTA JAKO SCIERKI')
-    INTO oIncydenty VALUES (5,(SELECT REF(k) FROM oKocury k WHERE k.pseudo='MALA'), 'CHYTRUSEK', '2007-03-07 ', 'ZALECAL SIE')
-    INTO oIncydenty VALUES (6,(SELECT REF(k) FROM oKocury k WHERE k.pseudo='TYGRYS'), 'DZIKI BILL', '2007-06-12 ', 'USILOWAL POZBAWIC ZYCIA')
-    INTO oIncydenty VALUES (7,(SELECT REF(k) FROM oKocury k WHERE k.pseudo='BOLEK'), 'DZIKI BILL', '2007-11-10 ', 'ODGRYZL UCHO')
-    INTO oIncydenty VALUES (8,(SELECT REF(k) FROM oKocury k WHERE k.pseudo='LASKA'), 'DZIKI BILL', '2008-12-12 ', 'POGRYZL ZE LEDWO SIE WYLIZALA')
-    INTO oIncydenty VALUES (9,(SELECT REF(k) FROM oKocury k WHERE k.pseudo='LASKA'), 'KAZIO', '2009-01-07 ', 'ZLAPAL ZA OGON I ZROBIL WIATRAK')
-    INTO oIncydenty VALUES (10,(SELECT REF(k) FROM oKocury k WHERE k.pseudo='DAMA'), 'KAZIO', '2009-02-07 ', 'CHCIAL OBEDRZEC ZE SKORY')
-    INTO oIncydenty VALUES (11,(SELECT REF(k) FROM oKocury k WHERE k.pseudo='MAN'), 'REKSIO', '2009-04-14 ', 'WYJATKOWO NIEGRZECZNIE OBSZCZEKAL')
-    INTO oIncydenty VALUES (12,(SELECT REF(k) FROM oKocury k WHERE k.pseudo='LYSY'), 'BETHOVEN', '2009-05-11 ', 'NIE PODZIELIL SIE SWOJA KASZA')
-    INTO oIncydenty VALUES (13,(SELECT REF(k) FROM oKocury k WHERE k.pseudo='RURA'), 'DZIKI BILL', '2009-09-03 ', 'ODGRYZL OGON')
-    INTO oIncydenty VALUES (14,(SELECT REF(k) FROM oKocury k WHERE k.pseudo='PLACEK'), 'BAZYLI', '2010-07-12 ', 'DZIOBIAC UNIEMOZLIWIL PODEBRANIE KURCZAKA')
-    INTO oIncydenty VALUES (15,(SELECT REF(k) FROM oKocury k WHERE k.pseudo='PUSZYSTA'), 'SMUKLA', '2010-11-19 ', 'OBRZUCILA SZYSZKAMI')
-    INTO oIncydenty VALUES (16,(SELECT REF(k) FROM oKocury k WHERE k.pseudo='KURKA'), 'BUREK', '2010-12-14 ', 'POGONIL')
-    INTO oIncydenty VALUES (17,(SELECT REF(k) FROM oKocury k WHERE k.pseudo='MALY'), 'CHYTRUSEK', '2011-07-13 ', 'PODEBRAL PODEBRANE JAJKA')
-    INTO oIncydenty VALUES (18,(SELECT REF(k) FROM oKocury k WHERE k.pseudo='UCHO'), 'SWAWOLNY DYZIO', '2011-07-14 ', 'OBRZUCIL KAMIENIAMI')
-SELECT * FROM dual;
+INSERT ALL INTO oincydenty
+VALUES (1, (SELECT REF(k) FROM okocury k WHERE k.pseudo = 'TYGRYS'), 'KAZIO', '2004-10-13 ', 'USILOWAL NABIC NA WIDLY')
+    INTO oincydenty
+VALUES (2,
+        (SELECT REF(k) FROM okocury k WHERE k.pseudo = 'ZOMBI'),
+        'SWAWOLNY DYZIO',
+        '2005-03-07 ',
+        'WYBIL OKO Z PROCY')
+    INTO oincydenty
+VALUES (3, (SELECT REF(k) FROM okocury k WHERE k.pseudo = 'BOLEK'), 'KAZIO', '2005-03-29 ', 'POSZCZUL BURKIEM')
+    INTO oincydenty
+VALUES (4,
+        (SELECT REF(k) FROM okocury k WHERE k.pseudo = 'SZYBKA'),
+        'GLUPIA ZOSKA',
+        '2006-09-12 ',
+        'UZYLA KOTA JAKO SCIERKI')
+    INTO oincydenty
+VALUES (5, (SELECT REF(k) FROM okocury k WHERE k.pseudo = 'MALA'), 'CHYTRUSEK', '2007-03-07 ', 'ZALECAL SIE')
+    INTO oincydenty
+VALUES (6,
+        (SELECT REF(k) FROM okocury k WHERE k.pseudo = 'TYGRYS'),
+        'DZIKI BILL',
+        '2007-06-12 ',
+        'USILOWAL POZBAWIC ZYCIA')
+    INTO oincydenty
+VALUES (7, (SELECT REF(k) FROM okocury k WHERE k.pseudo = 'BOLEK'), 'DZIKI BILL', '2007-11-10 ', 'ODGRYZL UCHO')
+    INTO oincydenty
+VALUES (8,
+        (SELECT REF(k) FROM okocury k WHERE k.pseudo = 'LASKA'),
+        'DZIKI BILL',
+        '2008-12-12 ',
+        'POGRYZL ZE LEDWO SIE WYLIZALA')
+    INTO oincydenty
+VALUES (9,
+        (SELECT REF(k) FROM okocury k WHERE k.pseudo = 'LASKA'),
+        'KAZIO',
+        '2009-01-07 ',
+        'ZLAPAL ZA OGON I ZROBIL WIATRAK')
+    INTO oincydenty
+VALUES (10, (SELECT REF(k) FROM okocury k WHERE k.pseudo = 'DAMA'), 'KAZIO', '2009-02-07 ', 'CHCIAL OBEDRZEC ZE SKORY')
+    INTO oincydenty
+VALUES (11,
+        (SELECT REF(k) FROM okocury k WHERE k.pseudo = 'MAN'),
+        'REKSIO',
+        '2009-04-14 ',
+        'WYJATKOWO NIEGRZECZNIE OBSZCZEKAL')
+    INTO oincydenty
+VALUES (12,
+        (SELECT REF(k) FROM okocury k WHERE k.pseudo = 'LYSY'),
+        'BETHOVEN',
+        '2009-05-11 ',
+        'NIE PODZIELIL SIE SWOJA KASZA')
+    INTO oincydenty
+VALUES (13, (SELECT REF(k) FROM okocury k WHERE k.pseudo = 'RURA'), 'DZIKI BILL', '2009-09-03 ', 'ODGRYZL OGON')
+    INTO oincydenty
+VALUES (14,
+        (SELECT REF(k) FROM okocury k WHERE k.pseudo = 'PLACEK'),
+        'BAZYLI',
+        '2010-07-12 ',
+        'DZIOBIAC UNIEMOZLIWIL PODEBRANIE KURCZAKA')
+    INTO oincydenty
+VALUES (15, (SELECT REF(k) FROM okocury k WHERE k.pseudo = 'PUSZYSTA'), 'SMUKLA', '2010-11-19 ', 'OBRZUCILA SZYSZKAMI')
+    INTO oincydenty
+VALUES (16, (SELECT REF(k) FROM okocury k WHERE k.pseudo = 'KURKA'), 'BUREK', '2010-12-14 ', 'POGONIL')
+    INTO oincydenty
+VALUES (17,
+        (SELECT REF(k) FROM okocury k WHERE k.pseudo = 'MALY'),
+        'CHYTRUSEK',
+        '2011-07-13 ',
+        'PODEBRAL PODEBRANE JAJKA')
+    INTO oincydenty
+VALUES (18,
+        (SELECT REF(k) FROM okocury k WHERE k.pseudo = 'UCHO'),
+        'SWAWOLNY DYZIO',
+        '2011-07-14 ',
+        'OBRZUCIL KAMIENIAMI')
+SELECT *
+FROM dual;
 --</editor-fold>
 --</editor-fold>
 --</editor-fold>
 --<editor-fold desc="przykłady zapytan">
 --Obecna ilość myszy na koncie
-SELECT k.getWlasciciel().getDisplayName() "kot",
-       COUNT(*) "Ilosc myszy"
+SELECT k.getWlasciciel().getDisplayName() "kot", COUNT(*) "ilosc myszy"
 FROM okonta k
-WHERE k.isOn()=1
+WHERE k.isOn() = 1
 GROUP BY k.getWlasciciel();
 --plebsy które nie są sługami
 SELECT p.getkocur().getDisplayName() "szczesliwy plebs"
 FROM oplebs p
-WHERE p.getKocur() NOT IN (
-    SELECT e.getSluga() FROM oelita e
-    );
+WHERE p.getKocur() NOT IN (SELECT e.getSluga() FROM oelita e);
 --</editor-fold>
 --<editor-fold desc="Zad 17">
 SELECT k.getDisplayName() "kot", k.zjadarazem() "spozycie", k.getnazwabandy() "banda"
@@ -616,28 +647,248 @@ ORDER BY k.zjadaRazem() DESC;
 --</editor-fold>
 --</editor-fold>
 
-CREATE TABLE plebs (
-    pseudo VARCHAR2(15),
+--<editor-fold desc="Zad 48">
+
+--<editor-fold desc="Tablice">
+DROP TABLE kontatab;
+DROP TABLE elitatab;
+DROP TABLE plebstab;
+
+CREATE TABLE plebstab (
+    id_plebsu NUMBER,
+    pseudo    VARCHAR2(15)
+        CONSTRAINT ple_fk_ps REFERENCES kocury (pseudo),
+    CONSTRAINT ple_pk PRIMARY KEY (id_plebsu)
+);
+INSERT ALL INTO plebstab
+VALUES (1, 'SZYBKA')
+    INTO plebstab
+VALUES (2, 'BOLEK')
+    INTO plebstab
+VALUES (3, 'LASKA')
+    INTO plebstab
+VALUES (4, 'MAN')
+    INTO plebstab
+VALUES (5, 'DAMA')
+    INTO plebstab
+VALUES (6, 'PLACEK')
+    INTO plebstab
+VALUES (7, 'RURA')
+    INTO plebstab
+VALUES (8, 'ZERO')
+    INTO plebstab
+VALUES (9, 'PUSZYSTA')
+    INTO plebstab
+VALUES (10, 'UCHO')
+    INTO plebstab
+VALUES (11, 'MALY')
+    INTO plebstab
+VALUES (12, 'MALA')
+SELECT *
+FROM dual;
+
+CREATE TABLE elitatab (
+    id_elity NUMBER,
+    pseudo   VARCHAR2(15)
+        CONSTRAINT e_ps_fk REFERENCES kocury (pseudo),
+    sluga    NUMBER
+        CONSTRAINT e_sl_fk REFERENCES plebstab (id_plebsu),
+    CONSTRAINT e_pk PRIMARY KEY (id_elity)
+);
+INSERT ALL INTO elitatab
+VALUES (1, 'TYGRYS', 5)
+    INTO elitatab
+VALUES (2, 'LOLA', 9)
+    INTO elitatab
+VALUES (3, 'ZOMBI', 4)
+    INTO elitatab
+VALUES (4, 'LYSY', 1)
+    INTO elitatab
+VALUES (5, 'KURKA', 10)
+    INTO elitatab
+VALUES (6, 'RAFA', 7)
+SELECT *
+FROM dual;
+
+CREATE TABLE kontatab (
+    id_konta          NUMBER,
+    wlasciciel        NUMBER
+        CONSTRAINT kon_fk_wl REFERENCES elitatab (id_elity),
+    data_wprowadzenia DATE
+        CONSTRAINT kon_n_dw CHECK (data_wprowadzenia IS NOT NULL),
+    data_usuniecia    DATE,
+    CONSTRAINT kon_ch_du CHECK (data_wprowadzenia <= data_usuniecia),
+    CONSTRAINT kon_pk PRIMARY KEY (id_konta)
+);
+INSERT ALL INTO kontatab
+VALUES (1, 1, '2019-01-01', '2019-01-08')
+    INTO kontatab
+VALUES (2, 2, '2019-01-02', '2019-01-05')
+    INTO kontatab
+VALUES (3, 1, '2019-01-03', '2019-01-03')
+    INTO kontatab
+VALUES (4, 3, '2019-01-04', '2019-01-08')
+    INTO kontatab
+VALUES (5, 4, '2019-01-05', NULL)
+    INTO kontatab
+VALUES (6, 5, '2019-01-06', NULL)
+    INTO kontatab
+VALUES (7, 1, '2019-01-07', '2019-01-11')
+    INTO kontatab
+VALUES (8, 2, '2019-01-08', NULL)
+    INTO kontatab
+VALUES (9, 2, '2019-01-09', NULL)
+    INTO kontatab
+VALUES (10, 1, '2019-01-10', '2019-01-13')
+    INTO kontatab
+VALUES (11, 6, '2019-01-11', NULL)
+    INTO kontatab
+VALUES (12, 1, '2019-01-12', NULL)
+    INTO kontatab
+VALUES (13, 3, '2019-01-13', NULL)
+    INTO kontatab
+VALUES (14, 5, '2019-01-14', '2019-01-15')
+SELECT *
+FROM dual;
+
+--</editor-fold>
+
+
+--<editor-fold desc="Perspektywy">
+
+CREATE OR REPLACE FORCE VIEW nakocury
+        OF KOCUR
+    WITH OBJECT IDENTIFIER (pseudo) AS
+    SELECT imie,
+           plec,
+           pseudo,
+           funkcja,
+           MAKE_REF(nakocury, szef) szef,
+           w_stadku_od,
+           przydzial_myszy,
+           myszy_ekstra,
+           nr_bandy
+    FROM kocury;
+
+CREATE OR REPLACE VIEW naplebs
+        OF PLEBS
+    WITH OBJECT IDENTIFIER (nr_plebsu) AS
+    SELECT id_plebsu, MAKE_REF(nakocury, pseudo) kocur
+    FROM plebstab;
+
+CREATE OR REPLACE VIEW naelity
+        OF ELITA
+    WITH OBJECT IDENTIFIER (nr_elity) AS
+    SELECT id_elity, MAKE_REF(nakocury, pseudo) kocur, MAKE_REF(naplebs, sluga) sluga
+    FROM elitatab;
+
+CREATE OR REPLACE VIEW nakonta
+        OF KONTO
+    WITH OBJECT IDENTIFIER (id_konta) AS
+    SELECT kontatab.id_konta, MAKE_REF(naelity, wlasciciel) wlasciciel, data_wprowadzenia, data_usuniecia
+    FROM kontatab;
+--</editor-fold>
+
+--<editor-fold desc="Zadania">
+
+--Obecna ilość myszy na koncie
+SELECT k.wlasciciel "kot", COUNT(*) "ilosc myszy"
+FROM kontatab k
+WHERE k.data_usuniecia IS NOT NULL
+GROUP BY k.wlasciciel;
+
+--plebsy które nie są sługami
+SELECT p.pseudo "szczesliwy plebs"
+FROM plebstab p
+WHERE p.id_plebsu NOT IN (SELECT sluga FROM elitatab);
+
+--Zad 17
+SELECT k.pseudo "kot", k.zjadarazem() "spozycie", k.getnazwabandy() "banda"
+FROM nakocury k
+WHERE k.GetTeren() IN ('POLE', 'CALOSC');
+
+--Zad 23
+SELECT k.pseudo "kot", k.getSpozycie() "zjada rocznie"
+FROM nakocury k
+WHERE k.maPremie() = 1
+ORDER BY k.zjadaRazem() DESC;
+
+--</editor-fold>
+
+--</editor-fold>
+
+--<editor-fold desc="Zad 49">
+drop table myszy;
+
+DECLARE
+    waga_min NUMBER DEFAULT 15;
+    waga_max NUMBER DEFAULT 35;
+    PRAGMA AUTONOMOUS_TRANSACTION;
+    query VARCHAR2(2048);
+BEGIN
+    query := '
+    CREATE TABLE myszy (
+        nr_myszy       NUMBER GENERATED BY DEFAULT ON NULL AS IDENTITY CONSTRAINT my_pk PRIMARY KEY,
+        lowca          VARCHAR(15) CONSTRAINT my_lw_fk REFERENCES kocury (pseudo),
+        zjadacz        VARCHAR(15) CONSTRAINT my_zj_fk REFERENCES kocury (pseudo),
+        waga_myszy     NUMBER NOT NULL CONSTRAINT my_wm_ck CHECK (waga_myszy BETWEEN '||waga_min||' AND '||waga_max||'),
+        data_zlowienia DATE   NOT NULL,
+        data_wydania   DATE
+    )';
+    EXECUTE IMMEDIATE query;
+    COMMIT;
+END;
+
+DECLARE
+    kiedy DATE DEFAULT '2004-01-01';
+    sroda  DATE;
+    dzien NUMBER;
+    miesiac NUMBER;
+    TYPE TabelaPseudo IS TABLE OF Kocury.PSEUDO%TYPE INDEX BY BINARY_INTEGER;
+    pseuda TabelaPseudo;
+    queryString VARCHAR2(2048);
+    ile NUMBER;
+BEGIN
     
-CONSTRAINT oopl_pk PRIMARY KEY (nr_plebsu
-),
-CONSTRAINT oopl_sc_k kot SCOPE IS OKocury
-);
+    sroda := kiedy;
+    WHILE kiedy <= '2016-01-18' LOOP
+        SELECT EXTRACT(MONTH FROM kiedy) INTO miesiac FROM DUAL;
+        SELECT EXTRACT(DAY FROM kiedy) INTO dzien FROM DUAL;
+        sroda := (next_day(last_day(add_months(sroda, 1))-7, 'Środa'));
+        
+        queryString := 'SELECT pseudo FROM Kocury WHERE w_stadku_od < ' || q'[']' || kiedy || q'[']';
+        EXECUTE IMMEDIATE queryString BULK COLLECT INTO pseuda;
+        
+        SELECT SUM(NVL(przydzial_myszy,0) + NVL(myszy_extra,0)) INTO ile
+        FROM Kocury WHERE w_stadku_od < sroda;
+        
+        FOR i IN 1..ile LOOP
+            INSERT INTO Myszy(lowca, zjadacz, waga_myszy, data_zlowania, data_wydania)
+            VALUES (
+                       pseuda(DMBS_RANDOM.VALUE(1, psueda.COUNT)),
+                       NULL,
+                       DBMS_RANDOM.VALUE(50, 100),
+                       kiedy + DBMS_RANDOM.VALUE(0, 27),
+                       NULL
+                       );
+        END LOOP;
+        
+        FOR kot IN (
+        SELECT
+               pseudo,
+               NVL(przydzial_myszy, 0) + NVL(myszy_extra, 0) zjada
+        FROM kocury ORDER BY zjada DESC
+        ) LOOP
+            
+            UPDATE Myszy SET zjadacz=kot.pseudo
+            WHERE zjadacz IS NULL AND ROWNUM <= kot.zjada;
+        
+        END LOOP;
+        
+        COMMIT;
+        
+        kiedy := last_day(kiedy) + 1;
+    END LOOP;
 
-CREATE TABLE elita (
-CONSTRAINT ooe_pk PRIMARY KEY (nr_elity
-),
-CONSTRAINT ooe_sc_k kot SCOPE IS OKocury,
-CONSTRAINT ooe_sc_s sluga SCOPE IS OPlebs
-);
-
-CREATE TABLE konta (
-CONSTRAINT ookon_pk PRIMARY KEY (id_konta
-),
-CONSTRAINT ookon_sc_w wlasciciel SCOPE IS Oelita,
-CONSTRAINT ookon_dw CHECK (data_wprowadzenia IS NOT NULL
-),
-CONSTRAINT ookon_du CHECK (data_wprowadzenia <= data_usuniecia
-)
-);
+END;
 --</editor-fold>
